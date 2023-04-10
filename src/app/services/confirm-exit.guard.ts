455b181e8300ea257570aead7f1ea88799980ccb
Moe Zaky <devmuzaky@gmail.com>
2023-04-10 14:25:30 +0200

    implementing Deactivate guard

---
+++ src/app/services/confirm-exit.guard.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angular/router';
+import {CourseComponent} from "../courses/course/course.component";
+
+@Injectable()
+export class ConfirmExitGuard implements CanDeactivate<CourseComponent> {
+  canDeactivate(component: CourseComponent, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState: RouterStateSnapshot): boolean {
+    return component.confirmExit()
+  }
+
+
+}
