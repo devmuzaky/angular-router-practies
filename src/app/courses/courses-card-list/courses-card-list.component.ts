84b43745219fa7a0bedec7a3b6d270fdd71e2143
Moe Zaky <devmuzaky@gmail.com>
2023-04-10 21:56:46 +0200

    implementing query parameters

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Course} from '../model/course';
-import {MatDialog,  MatDialogConfig} from '@angular/material/dialog';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import {filter, tap} from 'rxjs/operators';
 
@@ -11,41 +11,31 @@ import {filter, tap} from 'rxjs/operators';
 })
 export class CoursesCardListComponent implements OnInit {
 
-  @Input()
-  courses: Course[] = [];
+  @Input() courses: Course[] = [];
 
-  @Output()
-  private coursesChanged = new EventEmitter();
+  @Output() private coursesChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) {
-
   }
 
   ngOnInit() {
-
   }
 
-    editCourse(course: Course) {
+  editCourse(course: Course) {
 
-        const dialogConfig = new MatDialogConfig();
+    const dialogConfig = new MatDialogConfig();
 
-        dialogConfig.disableClose = true;
-        dialogConfig.autoFocus = true;
-        dialogConfig.width = "400px";
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
 
-        dialogConfig.data = course;
+    dialogConfig.data = course;
 
-        const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
 
-        dialogRef.afterClosed()
-            .pipe(
-                filter(val => !!val),
-                tap(() => this.coursesChanged.emit())
+    dialogRef.afterClosed()
+      .pipe(filter(val => !!val), tap(() => this.coursesChanged.emit()))
+      .subscribe();
 
-            )
-            .subscribe();
-
-
-    }
-
+  }
 }
