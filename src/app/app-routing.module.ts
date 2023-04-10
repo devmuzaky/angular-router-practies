1f383dda058041437afb54da99cacd1363f19bfa
Moe Zaky <devmuzaky@gmail.com>
2023-04-10 22:03:49 +0200

    enabling tracing in the routing configuration object

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -47,7 +47,8 @@ const routes: Routes = [
     RouterModule.forRoot(
       routes,
       {
-        preloadingStrategy: CustomPreloadingStrategy
+        preloadingStrategy: CustomPreloadingStrategy,
+        enableTracing: true
       }
     )
   ],
